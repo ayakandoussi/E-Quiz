b4427eab969db001742790ca10396fa73d4ea60d
ayakandoussi <ayakandoussi1905@gmail.com>
2024-12-26 23:50:10 +0100

    creation des classes

---
+++ E_Quiz/src/main/java/com/projetjava/domain/Utilisateur.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.projetjava.domain;
+
+/**
+ *
+ * @author User
+ */
+public abstract class Utilisateur {
+    private static int id=0;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String motDePasse;
+    private String role;
+
+    public Utilisateur(String nom, String prenom, String email, String motDePasse, String role) {
+        this.id++;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.motDePasse = motDePasse;
+        this.role = role;
+    }
+
+    public static int getId() {
+        return id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setMotDePasse(String motDePasse) {
+        this.motDePasse = motDePasse;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
+
+    public abstract void afficher();
+    
+}
