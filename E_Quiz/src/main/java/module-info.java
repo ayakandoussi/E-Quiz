a4c0146f36562e0f10a4e9c77c899e4c8c0bd1fb
zakariaeLA <lachhabzakariae@gmail.com>
2024-12-29 16:17:43 +0000

    ..

--- E_Quiz/src/main/java/module-info.java
+++ E_Quiz/src/main/java/module-info.java
@@ -3,6 +3,8 @@ module com.projetjava {
     requires javafx.fxml;
     requires java.base;
     requires java.sql;
+    requires javafx.base;
+    requires javafx.graphics;
 
     opens com.projetjava to javafx.fxml;
     exports com.projetjava;
