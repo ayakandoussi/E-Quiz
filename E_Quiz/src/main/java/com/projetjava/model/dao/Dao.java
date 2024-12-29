de2438b22ea889863bb490fda88d97366e2a7a42
ayakandoussi <ayakandoussi1905@gmail.com>
2024-12-29 10:41:37 +0100

    les classes U/E/P .

--- E_Quiz/src/main/java/com/projetjava/model/dao/Dao.java
+++ E_Quiz/src/main/java/com/projetjava/model/dao/Dao.java
@@ -1,13 +1,1 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
-package com.projetjava.model.dao;
 
-/**
- *
- * @author HP_Maroua
- */
-public interface Dao {
-    
-}
