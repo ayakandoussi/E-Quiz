90496103469d740a143a954557ea4c623d214f4f
zakariaeLA <lachhabzakariae@gmail.com>
2024-12-29 10:36:24 +0000

    Interface Dao

--- E_Quiz/src/main/java/com/projetjava/model/dao/Dao.java
+++ E_Quiz/src/main/java/com/projetjava/model/dao/Dao.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.projetjava.model.dao;
 
-/**
- *
- * @author HP_Maroua
- */
-public interface Dao {
-    
+import java.util.List;
+
+public interface Dao<T> {
+
+    void add(T objet);
+
+    T getById(int id);
+
+    List<T> getAll();
+
+    void update(T objet);
+
+    void delete(int id);
 }
