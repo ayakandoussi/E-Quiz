79b9588aae23ec7b6d9dbd763d2d811bd1df0041
zakariaeLA <lachhabzakariae@gmail.com>
2024-12-29 10:06:19 +0000

    modif BDDConnexion

--- E_Quiz/src/main/java/com/projetjava/model/dao/impl/BDConnexion.java
+++ E_Quiz/src/main/java/com/projetjava/model/dao/impl/BDConnexion.java
@@ -4,12 +4,25 @@ package com.projetjava.model.dao.impl;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
 public class BDConnexion {
+
     private static final String URL = "jdbc:mysql://localhost:3306/equiz";
     private static final String USER = "root";
     private static final String PASSWORD = "ABC@def123";
+    private Connection connection;
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+    public BDConnexion() throws SQLException {
+        connection = DriverManager.getConnection(URL, USER, PASSWORD);
     }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void close() throws SQLException {
+        connection.close();
+    }
+
+
 }
--- E_Quiz/src/main/java/com/projetjava/model/dao/impl/TestBDConnexion.java
+++ E_Quiz/src/main/java/com/projetjava/model/dao/impl/TestBDConnexion.java
@@ -4,18 +4,27 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class TestBDConnexion {
-
     public static void main(String[] args) {
+        BDConnexion bdConnexion = null;
         try {
-            // Test de la connexion
-            Connection conn = BDConnexion.getConnection();
-            if (conn != null) {
-                System.out.println("Connexion reussie !");
-            } else {
-                System.out.println("Connexion echouee.");
+
+            bdConnexion = new BDConnexion();
+            Connection connection = bdConnexion.getConnection();
+
+            if (connection != null) {
+                System.out.println("Connexion réussie à la base de données !");
             }
         } catch (SQLException e) {
             System.out.println("Erreur de connexion : " + e.getMessage());
+        } finally {
+            if (bdConnexion != null) {
+                try {
+                    bdConnexion.close();
+                    System.out.println("Connexion fermée !");
+                } catch (SQLException e) {
+                    System.out.println("Erreur lors de la fermeture : " + e.getMessage());
+                }
+            }
         }
     }
 }
