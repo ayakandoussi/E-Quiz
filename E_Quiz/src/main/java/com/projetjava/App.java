722fa710f363564d274242e7b685328116d22d23
zakariaeLA <lachhabzakariae@gmail.com>
2024-12-30 17:42:40 +0000

    modif

--- E_Quiz/src/main/java/com/projetjava/App.java
+++ E_Quiz/src/main/java/com/projetjava/App.java
@@ -8,16 +8,14 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-/**
- * JavaFX App
- */
+
 public class App extends Application {
 
     private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("PagePrincipaleEtudiant"), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
