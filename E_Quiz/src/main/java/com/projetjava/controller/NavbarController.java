c033fd925022f75413a8bb295aaf8d3fcc6c37b9
ayakandoussi <ayakandoussi1905@gmail.com>
2024-12-31 11:59:15 +0100

    hashage dans DAO et classe Session

---
+++ E_Quiz/src/main/java/com/projetjava/controller/NavbarController.java
@@ -0,0 +1,26 @@
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.Pane;
+
+public class NavbarController {
+
+    @FXML
+    private Pane NavbarController;
+
+    @FXML
+    private MenuButton menu;
+    @FXML
+    private MenuItem Accueil;
+    @FXML
+    private MenuItem Profil;
+    @FXML
+    private MenuItem SeDeconnecter;
+
+    
+    @FXML
+    private Label rolelabel;
+
+}
