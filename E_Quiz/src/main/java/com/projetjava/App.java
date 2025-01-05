edd0d844be0a50238bd17ad110023981f2068aec
zakariaeLA <lachhabzakariae@gmail.com>
2025-01-05 16:45:55 +0000

    logo + titre de l'App

--- E_Quiz/src/main/java/com/projetjava/App.java
+++ E_Quiz/src/main/java/com/projetjava/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,11 +15,14 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-       scene = new Scene(loadFXML("/com/projetjava/view/pages/LoginPage"), 800, 500);
-       //scene = new Scene(loadFXML("/com/projetjava/view/pages/AjoutQuiz"), 600, 400);
+        scene = new Scene(loadFXML("/com/projetjava/view/pages/LoginPage"), 800, 500);
+        //scene = new Scene(loadFXML("/com/projetjava/view/pages/AjoutQuiz"), 600, 400);
         //scene = new Scene(loadFXML("QuizPage"), 640, 480);//
-         //scene = new Scene(loadFXML("Accueil"), 640, 480);
+        //scene = new Scene(loadFXML("EtudiantAccueil"), 640, 480);
+        Image logo = new Image(getClass().getResourceAsStream("/com/projetjava/view/images/logo.png"));
+        stage.getIcons().add(logo); 
         stage.setScene(scene);
+        stage.setTitle("E-Quiz");
         stage.show();
     }
 
@@ -27,9 +31,9 @@ public class App extends Application {
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-         // FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/com/projetjava/view/pages/AjoutQuiz.fxml"));
-       FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/com/projetjava/view/pages/LoginPage.fxml"));
-        //FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/com/projetjava/view/pages/Accueil.fxml"));
+        //FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/com/projetjava/view/pages/AjoutQuiz.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/com/projetjava/view/pages/LoginPage.fxml"));
+        // FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/com/projetjava/view/pages/EtudiantAccueil.fxml"));
 
         return fxmlLoader.load();
     }
